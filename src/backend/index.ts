ab51e1539b1756cba2f8104f6196e718631af997
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2017-12-22 22:21:26 +0200

    Change backend port

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,9 +1,8 @@
 import * as path from 'path';
 import * as express from 'express'; 
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8000;
 const app = express();
 
-// app.use(express.static(path.join(__dirname, '../frontend')));
-app.use((req, res) => res.send('ok'));
+app.use(express.static(path.join(__dirname, '../frontend')));
 app.listen(PORT, () => console.log(`Backend is started at ${PORT}`));
